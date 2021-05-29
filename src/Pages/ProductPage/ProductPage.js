44d11267c4a9b6ae3949f2886f85a2efbcb87dfe
saifur116 <67361625+saifur116@users.noreply.github.com>
2021-05-29 12:59:49 +0600

    first commit

--- src/Pages/ProductPage/ProductPage.js
+++ src/Pages/ProductPage/ProductPage.js
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./ProductPage.module.scss";
 
 function ProductPage() {
-  return <div className={styles.pageWrapper}>Develop Product Page Here</div>;
+  return <div className={styles.pageWrapper}>hello product</div>;
 }
 
 export default ProductPage;
